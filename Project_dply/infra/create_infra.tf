2e0a9d41bd5f2de3a69ae6f0bd5e3532016e93a7
Guna <143094621+itsu-me-guna@users.noreply.github.com>
2026-08-23 00:36:10 +0530

    Refactor Terraform script to use existing resources

--- Project_dply/infra/create_infra.tf
+++ Project_dply/infra/create_infra.tf
@@ -2,40 +2,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Look up an existing bucket instead of creating one
+# Look up existing bucket
 data "aws_s3_bucket" "glue_scripts" {
   bucket = "my-glue-scripts-bucket-from-tf-20260823-000001"
 }
 
+# Upload script into that bucket
 resource "aws_s3_object" "script" {
   bucket = data.aws_s3_bucket.glue_scripts.id
   key    = "scripts/My_Source_ingest.py"
   source = "src/My_Source_ingest.py"
 }
 
-resource "aws_iam_role" "glue_role" {
+# Look up existing IAM role
+data "aws_iam_role" "glue_role" {
   name = "glue-job-role-guna-from-tf"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "glue.amazonaws.com"
-      }
-    }]
-  })
 }
 
 resource "aws_iam_role_policy_attachment" "glue_policy_attach" {
-  role       = aws_iam_role.glue_role.name
+  role       = data.aws_iam_role.glue_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
 
 resource "aws_glue_job" "my_job" {
   name     = "my-source-ingest-job"
-  role_arn = aws_iam_role.glue_role.arn
+  role_arn = data.aws_iam_role.glue_role.arn
 
   command {
     name            = "glueetl"
