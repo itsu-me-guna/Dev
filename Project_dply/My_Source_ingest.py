348bdcea78d1ad4491bea4fe56fc6ad036c9cffd
Guna <143094621+itsu-me-guna@users.noreply.github.com>
2026-08-22 23:04:31 +0530

    Delete Project_dply directory

--- Project_dply/My_Source_ingest.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from Library.get_my_spark import *
-
-spark = this_is_my_spark("MySparkApplication")
-
-print(spark)
-# customer
-spark.read.format("csv").options(header=True, nullValue="NULL", inferSchema=True ).load(r"E:\Class\Project\sample_files\001_sales_copy.csv").show()
