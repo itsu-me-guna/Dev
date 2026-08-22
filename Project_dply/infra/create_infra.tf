8957165e9c9708c21663381fe865c760f616c7f4
Guna <143094621+itsu-me-guna@users.noreply.github.com>
2026-08-22 23:05:11 +0530

    Add files via upload

---
+++ Project_dply/infra/create_infra.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "us-east-1"   # change to your region
+}
+
+resource "aws_s3_bucket" "glue_scripts" {
+  bucket = "my-glue-scripts-bucket-20260822"   # must be globally unique
+}
+
+resource "aws_s3_object" "script" {
+  bucket = aws_s3_bucket.glue_scripts.id
+  key    = "scripts/My_Source_ingest.py"
+  source = "src\My_Source_ingest.py"   # local path to your file
+}
+
+resource "aws_iam_role" "glue_role" {
+  name = "glue-job-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "glue.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_policy_attach" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_glue_job" "my_job" {
+  name     = "my-source-ingest-job"
+  role_arn = aws_iam_role.glue_role.arn
+
+  command {
+    name            = "glueetl"
+    script_location = "s3://${aws_s3_bucket.glue_scripts.id}/${aws_s3_object.script.key}"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--TempDir" = "s3://${aws_s3_bucket.glue_scripts.id}/tmp/"
+    "--job-language" = "python"
+  }
+
+  max_capacity = 2
+}
